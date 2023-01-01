77b58eabbf2be820e722d9e6b6d23093345d2d77
ranafge@gmail.com <ranafge@gmail.com>
2023-01-02 03:49:19 +0600

    Base class add to login to TestLogin

--- SeleniumPageObjectModel/src/test/java/pageobjects/Login.java
+++ SeleniumPageObjectModel/src/test/java/pageobjects/Login.java
@@ -1,4 +1,33 @@
 package pageobjects;
 
-public class Login {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class Login extends Base{
+    private WebDriver driver;
+
+    By usernameLocator = By.id("username");
+    By passwordLocator = By.id("password");
+    By submitBtn = By.cssSelector("button");
+    By successMessageLocator = By.id(".flash.success");
+
+    public Login(WebDriver driver) {
+        super(driver);
+        visit("https://the-internet.herokuapp.com/login");
+    }
+
+    public void with(String username, String password) {
+        type(username, usernameLocator);
+        type(password, passwordLocator);
+
+        click(submitBtn);
+
+    }
+
+    public boolean successMessage() {
+        return driver.findElement(successMessageLocator).isDisplayed();
+    }
+
+
+
 }
--- SeleniumPageObjectModel/src/test/java/tests/TestLogin.java
+++ SeleniumPageObjectModel/src/test/java/tests/TestLogin.java
@@ -1,4 +1,41 @@
 package tests;
 
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import pageobjects.Login;
+
+
+
 public class TestLogin {
+
+    private WebDriver driver;
+    private Login login;
+
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments();
+        driver = new ChromeDriver(options);
+        login = new Login(driver);
+    }
+
+    @Test
+    public void succeeded() {
+       login.with("tomsmith","SuperSecretPassword!");
+       Assert.assertTrue("success message not present",login.successMessage());
+    }
+
+    @After
+    public  void tearDown() {
+        driver.quit();
+    }
+
 }
